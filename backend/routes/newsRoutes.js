7b70974b811f3fe57c4a136e58c0f2f73f0044a1
mesuvas <suvas.magar.69@gmail.com>
2025-03-13 14:10:07 +0545

    news routes protected.

--- backend/routes/newsRoutes.js
+++ backend/routes/newsRoutes.js
@@ -2,12 +2,22 @@ const express = require("express");
 const newsController = require("../controllers/newsController");
 const router = express.Router();
 const upload = require("../middleware/multerConfig");
+const isAuthenticated = require("../middleware/isAuthenticated");
 
-router.post("/news", upload.single("file"), newsController.createNews);
-router.get("/news", newsController.getAllNews);
-router.get("/news/:id", newsController.getNewsById);
-router.put("/news/:id", upload.single("file"), newsController.updateNews);
-
+router.post(
+  "/news",
+  isAuthenticated,
+  upload.single("file"),
+  newsController.createNews
+);
+router.get("/news", isAuthenticated, newsController.getAllNews);
+router.get("/news/:id", isAuthenticated, newsController.getNewsById);
+router.put(
+  "/news/:id",
+  isAuthenticated,
+  upload.single("file"),
+  newsController.updateNews
+);
 router.delete("/news/:id", newsController.deleteNews);
 
 module.exports = router;

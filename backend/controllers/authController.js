55cc2319f0013d83776647d32a96a2d32def6f78
mesuvas <suvas.magar.69@gmail.com>
2025-03-13 15:20:49 +0545

    refresh token implemented

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -3,7 +3,7 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 // User Registration
-exports.register = async (req, res) => {
+const register = async (req, res) => {
   try {
     const { email, password } = req.body;
     if (!email || !password) {
@@ -21,7 +21,7 @@ exports.register = async (req, res) => {
 };
 
 // User Login
-exports.login = async (req, res) => {
+const login = async (req, res) => {
   try {
     const { email, password } = req.body;
     const user = await User.findOne({ email });
@@ -30,11 +30,39 @@ exports.login = async (req, res) => {
       return res.status(401).json({ error: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ userId: user._id }, "secret_key", {
+    const accessToken = jwt.sign({ userId: user._id }, "secret_key", {
       expiresIn: "1h",
     });
-    res.json({ message: "Login Successful..", token });
+
+    const refreshToken = jwt.sign({ userId: user._id }, "refresh_secret_key", {
+      expiresIn: "7d",
+    });
+
+    res.json({ message: "Login Successful..", accessToken, refreshToken });
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+};
+
+const refreshToken = async (req, res) => {
+  const { refreshToken } = req.cookies; // Get refresh token from the cookie
+
+  if (!refreshToken) {
+    return res.status(401).json({ error: "No refresh token provided" });
+  }
+  jwt.verify(refreshToken, "refresh_secret_key", (err, user) => {
+    if (err) {
+      return res.status(403).json({ error: "Invalid refresh token" });
+    }
+    const newAccessToken = jwt.sign({ userId: user.userId }, "secret_key", {
+      expiresIn: "15m",
+    });
+    res.json({ accessToken: newAccessToken });
+  });
+};
+
+exports = {
+  register,
+  login,
+  refreshToken,
 };
